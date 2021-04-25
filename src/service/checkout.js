fb80aaef6c645c7f5dcc00e13230150c2fb3c8e7
pram <pratham01@users.noreply.github.com>
2021-04-25 20:30:09 +0530

    Optimize code

--- src/service/checkout.js
+++ src/service/checkout.js
@@ -2,47 +2,26 @@ const catalogue = require('../model/catalouge');
 
 exports.checkout = async function(req,res){
     let reqJSON = req.body;
-    let map = await populateMap(reqJSON);
-    let val = await calculatePrice(map);
-    res.send({"price":val});
+    let map =  new Map();
+    reqJSON.forEach(i=>map.set(i, map.get(i) == undefined?1: map.get(i)+1));
+    res.send({"price":await calculatePrice(map)});
     res.end();
     return;
 };
 
-async function populateMap(reqJSON){
-    let map = new Map();
-    for(let i=0;i<reqJSON.length;i++){
-        if(!map.has(reqJSON[i])){   
-            map.set(reqJSON[i],1);
-        } else{
-            map.set(reqJSON[i], map.get(reqJSON[i])+1);
-        }
-    }
-    return map;
-}
 async function calculatePrice(map){
     let finalPrice = 0;
     let catalougeProducts = catalogue.getCatalogue();
     for(let[key, value] of map){
-        for(let itr =0;itr<catalougeProducts.length;itr++){
-            if(catalougeProducts[itr]._id == key){
-                let discount = catalougeProducts[itr]._discount;
-                if(discount != undefined) {
-                    discount = discount.split(" ");
-                    if(value>=parseInt(discount[0])){
-                        let tempFreq = value/parseInt(discount[0]);
-                        finalPrice+=parseInt(tempFreq)* parseInt(discount[2]);
-                        let remainingProduct = value - (parseInt(tempFreq)* parseInt(discount[0]));
-                        finalPrice+=remainingProduct* parseInt(catalougeProducts[itr]._price);
-                    }   else{
-                        finalPrice+=value*parseInt(catalougeProducts[itr]._price);
-                    }
-                } else{
-                    finalPrice+= value*parseInt(catalougeProducts[itr]._price);
-                }
-                break;
+        catalougeProducts.forEach(product=>{
+            if(product._id == key){
+                let discount = product._discount != undefined ? product._discount.split(" ") : Number.MAX_VALUE ;
+                if( value>=parseInt(discount[0]))
+                    finalPrice+=parseInt(value/parseInt(discount[0]))* parseInt(discount[2]) + (value % parseInt(discount[0]))* parseInt(product._price);
+                 else
+                    finalPrice+=value*parseInt(product._price);
             }
-        }
+        });
     }
     return finalPrice;
 }
